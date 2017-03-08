993eefb8ddbff9354e7c79492c6884b98d4f5ee9
Duarte Patrício <duarte.carvalho@tecnico.ulisboa.pt>
2017-03-08 17:22:19 +0000

    Implemented calculate_hash.

--- src/share/vm/ng2c/method_bci_hashtable.cpp
+++ src/share/vm/ng2c/method_bci_hashtable.cpp
@@ -1,4 +1,5 @@
 # include "ng2c/method_bci_hashtable.hpp"
+# include "classfile/altHashing.hpp"
 
 MethodBciHashtable::MethodBciHashtable(int table_size)
   : Hashtable<gen_array_t, mtGC>(table_size, sizeof(MethodBciEntry)) {
@@ -18,5 +19,9 @@ MethodBciHashtable::add_entry(Method * m, int bci)
 unsigned int
 MethodBciHashtable::calculate_hash(Method * m, int bci)
 {
-
+  unsigned int hash = (unsigned int)AltHashing::murmur3_32(bci, (const jbyte*)m, sizeof(Method));
+#ifdef ASSERT
+  gclog_or_tty->print_cr("[ng2c hashing] " INTPTR_FORMAT, (intptr_t)hash);
+#endif //ASSERT
+  return hash;
 }
