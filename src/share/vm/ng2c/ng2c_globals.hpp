1bb1e2796923834ae0eb4cfa3211a70ae2d8c184
Duarte Patrício <duarte.carvalho@tecnico.ulisboa.pt>
2017-03-09 13:26:21 +0000

    Made some fixes to the unit of the n generational array -- now based on a wrapper class.

--- src/share/vm/ng2c/ng2c_globals.hpp
+++ src/share/vm/ng2c/ng2c_globals.hpp
@@ -1,8 +1,30 @@
 #ifndef SHARE_VM_NG2C_NG2C_GLOBALS_HPP
 #define SHARE_VM_NG2C_NG2C_GLOBALS_HPP
 
-typedef unsigned long * gen_array_t;
+# include "memory/allocation.inline.hpp"
+
+// forward definition
+class NGenerationArray;
+
+typedef unsigned long ngen_t;
 
 const static int NG2C_GEN_ARRAY_SIZE = 17;
+
+class NGenerationArray : public CHeapObj<mtGC>
+{
+ private:
+  ngen_t * _array;
+
+ public:
+  NGenerationArray() { _array = NEW_C_HEAP_ARRAY(ngen_t, NG2C_GEN_ARRAY_SIZE, mtGC); }
+  NGenerationArray(ngen_t * array) : _array(array) { }
+  NGenerationArray(const NGenerationArray& copy) : _array(copy.array()) { }
+
+  ngen_t * array() const { return _array; }
+  int size() const { return NG2C_GEN_ARRAY_SIZE; } // hard-coded for now
+  unsigned int new_hash (int seed) { return (unsigned int)0; } // placeholder
+  ngen_t at(int pos) const { return _array[pos]; }
+};
+ 
 
 #endif // SHARE_VM_NG2C_NG2C_GLOBALS_HPP
