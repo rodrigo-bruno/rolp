e3622b8de8f330853f2d7b28e11011a6e6ed4861
Duarte Patrício <duarte.carvalho@tecnico.ulisboa.pt>
2017-03-13 15:20:09 +0000

    Changed default ngen array size to the correct one.

--- src/share/vm/ng2c/ng2c_globals.hpp
+++ src/share/vm/ng2c/ng2c_globals.hpp
@@ -5,7 +5,7 @@
 
 typedef unsigned long ngen_t;
 
-const static int NG2C_GEN_ARRAY_SIZE = 17;
+const static int NG2C_GEN_ARRAY_SIZE = 16;
 
 class NGenerationArray : public CHeapObj<mtGC>
 {
@@ -18,16 +18,16 @@ class NGenerationArray : public CHeapObj<mtGC>
 
  public:
   NGenerationArray(uint hash) : _hash(hash)
-    { _array = NEW_C_HEAP_ARRAY(ngen_t, NG2C_GEN_ARRAY_SIZE, mtGC); }
+    { _array = NEW_C_HEAP_ARRAY(ngen_t, NG2C_GEN_ARRAY_SIZE + 1, mtGC); }
   NGenerationArray(ngen_t * array, uint hash) : _array(array), _hash(hash) { }
   NGenerationArray(const NGenerationArray& copy) : _array(copy.array()), _hash(copy.hash()) { }
 
   ngen_t * array() const { return _array; }
   uint     hash()  const { return _hash; }
-  int      size()  const { return NG2C_GEN_ARRAY_SIZE; } // hard-coded for now
+  int      size()  const { return NG2C_GEN_ARRAY_SIZE + 1; } // hard-coded for now
   ngen_t   at(int pos) const { return _array[pos]; }
-  ngen_t   get_target_gen() const { return at(size() - 1); }
-  ngen_t * get_target_gen_addr()  { return &_array[size() - 1]; }
+  ngen_t   get_target_gen() const { return at(size()); }
+  ngen_t * get_target_gen_addr()  { return &_array[size()]; }
 
   void   apply_delta (NGenerationArray * thread_arr);
   unsigned int new_hash (int seed) {
