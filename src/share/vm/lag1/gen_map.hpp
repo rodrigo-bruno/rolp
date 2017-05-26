1790dc7253ec3c1e76562f3f8b1619439acd6a3f
Duarte Patrício <dtcpatricio@users.noreply.github.com>
2017-05-26 12:52:37 +0100

    Cleared the gen_map.hpp of unused stuff.

--- src/share/vm/lag1/gen_map.hpp
+++ src/share/vm/lag1/gen_map.hpp
@@ -38,24 +38,16 @@ class GenMap : public CHeapObj<mtGC> {
 
   uint _next_gen_id;
   GrowableArray<GenEl*>* _bda_klass_names;
-  // static BDARegion* _region_data;
-  // int        region_data_sz;
 
   // parse the command line string BDAKlasses="..."
   void parse_from_string(const char* line, void (GenMap::* parse)(char*));
   void parse_from_line(char* line);
 
  public:
-  
 
   GenMap() { }
 
   void initialize();
-
-  // checks if a klass is bda type and returns the appropriate gen id
-  // static uint is_bda_klass(Klass* k);
-  // // Assembler version of is_bda_klass(Klass*)
-  // static void is_bda_klass_asm(JavaThread * java_thread, Klass* k);
 
   GrowableArray<GenEl*> * bda_klass_names() { return _bda_klass_names; }
   // Retrieves the number of gens for locality aware policy objects
@@ -70,29 +62,6 @@ class GenMap : public CHeapObj<mtGC> {
   void add_entry(Klass* k); // TODO : change to register_klass_entry(..)
   // print klass names
   void print_klass_names(outputStream * ostream);
-
-  // TODO FIXME : This is too see if any of these are needed.
-  // adds an entry for the general object space
-  // inline void add_other_entry(Klass* k);
-  // // adds an entry for one of the bda spaces
-  // inline void add_region_entry(Klass* k, BDARegion* r);
-  // // an accessor for the region_map which is used by set_klass(k) to assign a bda space
-  // static BDARegion* region_for_klass(Klass* k);
-  // // an accessor for the _region_data array which saves the uint wrapper
-  // static BDARegion* region_data() { return _region_data; }
-  // // an accessor for the elements in the _region_data array
-  // static BDARegion* region_elem(uint r) {
-  //   int idx = 0;
-  //   while(_region_data[idx].value() != r)
-  //     ++idx;
-  //   return &_region_data[idx];
-  // }
-  // // Fast accessor for no_region and the low_addr of the _region_data array
-  // static BDARegion* no_region_ptr() { return &_region_data[0]; }
-  // // Fast accessor for region_start (other's space)
-  // static BDARegion* region_start_ptr() { return &_region_data[1]; }
-  // // Fast accessor for the last element in the _region_data array
-  // static BDARegion* last_region_ptr() { return &_region_data[_region_data_sz - 1]; }
 
 };
 
